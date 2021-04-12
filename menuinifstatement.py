eb4df7a159cdc45a410a283e48e3bba8706388cd
nomie788 <nquresh88@hotmail.com>
2021-04-12 12:32:10 -0500

    Update menuinifstatement.py

--- menuinifstatement.py
+++ menuinifstatement.py
@@ -1,42 +1,41 @@
+Condition: bool = True
+
+
 def menu():
     print("[1] Carbon Dioxide")
     print("[2] Air")
     print("[3] Helium")
     print("[4] Hydrogen")
 
-menu()
-option = int(input("Enter your option: "))
-numberofseconds = float(input("Number of seconds traveled: "))
 
-if 0 < numberofseconds <= 30:
-    while option != 0:
-        if option == 1:
-            print("Option 1 has been called")
-            Totalmeters = 258.0 * numberofseconds
-            print("Total Meters: ", Totalmeters)
+while Condition:
 
-        elif option == 2:
-            print("Option 2 has been called.")
-            Totalmeters = 331.5 * numberofseconds
-            print("Total Meters: ", Totalmeters)
-
-        elif option == 3:
-            print("Option 3 has been called.")
-            Totalmeters = 972.0 * numberofseconds
-            print("Total Meters; ", Totalmeters)
-
-        elif option == 4:
-            print("Option 4 has been called.")
-            Totalmeters = 1270.0 * numberofseconds
-            print("Total Meters; ", Totalmeters)
-        else:
-            print("Invalid option")
-else:
-    print("Invalid Seconds")
-
-    print()
     menu()
-    option = int(input("Enter your option:"))
+
+    option = int(input("Enter your option: "))
     numberofseconds = float(input("Number of seconds traveled: "))
 
-print("Your Response is invalid. Thanks for using this program.   Goodbye.")
+    if 0 < numberofseconds <= 30 and option == 1:
+        print("Option 1 has been called")
+        Totalmeters = 258.0 * numberofseconds
+        print("Total Meters: ", Totalmeters)
+
+    elif 0 < numberofseconds <= 30 and option == 2:
+        print("Option 2 has been called.")
+        Totalmeters = 331.5 * numberofseconds
+        print("Total Meters: ", Totalmeters)
+
+    elif 0 < numberofseconds <= 30 and option == 3:
+        print("Option 3 has been called.")
+        Totalmeters = 972.0 * numberofseconds
+        print("Total Meters; ", Totalmeters)
+
+    elif 0 < numberofseconds <= 30 and option == 4:
+        print("Option 4 has been called.")
+        Totalmeters = 1270.0 * numberofseconds
+        print("Total Meters; ", Totalmeters)
+
+    else:
+        print("Invalid Seconds or option, please try again.")
+
+Condition: bool = False
